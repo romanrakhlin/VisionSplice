3a72c802c3e67ba73b5c12ead6ef59cea4093c2d
romanrakhlin <rrakhlin@gmail.com>
2024-02-24 21:30:07 +0100

    Add assets for onboarding

--- MyApp.swift
+++ MyApp.swift
@@ -5,7 +5,6 @@ struct MyApp: App {
     var body: some Scene {
         WindowGroup {
             ProjectsView()
-//            EditorView(sourceItem: UIImage(named: "Pumpkin")!)
                 .preferredColorScheme(.dark)
         }
     }
